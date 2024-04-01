00a5ae4ce1ea113def7363c13afe2bac7e9ef212
anukritisahu <anukritisahu@IN-GYSM2J3.Nagarro.local>
2024-04-02 03:00:06 +0530

    start commit

---
+++ DevOps/src/main/java/com/nagarro/DevOps/Controller/Controller.java
@@ -0,0 +1,25 @@
+package com.nagarro.DevOps.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/Hello")
+public class Controller {
+	
+	@GetMapping("/")
+	public String getHello(){
+		return "Hello Spring";
+	
+	}
+	
+		@GetMapping("/new")
+		public String getNew(){
+			return "New Spring";
+		
+		}
+	
+
+}
